59d0dcf3385ef581dcbffd284eeada70664eeb92
ilya roschikhmarov <ilrosch@mail.ru>
2025-10-30 17:04:31 +0300

    step 2: add argument fotmat

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,10 +7,11 @@ def main():
         description="Compares two configuration files and shows a difference.",
     )
 
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    args = parser.parse_args()
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
 
+    args = parser.parse_args()
     print(args)
 
 
