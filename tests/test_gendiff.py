fdc180543ce931ecbc18ff2261d091b8ca2abcac
ilya roschikhmarov <ilrosch@mail.ru>
2025-10-31 14:40:32 +0300

    step 6: add yml format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,12 +5,20 @@ from gendiff import generate_diff
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_gendiff_json():
-    file_path1 = os.path.join(current_dir, "test_data", "file1.json")
-    file_path2 = os.path.join(current_dir, "test_data", "file2.json")
+def test_gendiff():
+    file_path1_json = os.path.join(current_dir, "test_data", "file1.json")
+    file_path2_json = os.path.join(current_dir, "test_data", "file2.json")
+
+    file_path1_yml = os.path.join(current_dir, "test_data", "file1.yml")
+    file_path2_yml = os.path.join(current_dir, "test_data", "file2.yml")
+
     expected_path = os.path.join(current_dir, "test_data", "expected.txt")
 
     with open(expected_path) as f:
         expected = f.read()
-        actual = generate_diff(file_path1, file_path2)
-        assert actual == expected
+        res_json = generate_diff(file_path1_json, file_path2_json)
+        assert res_json == expected
+        res_yml = generate_diff(file_path1_yml, file_path2_yml)
+        assert res_yml == expected
+        res_json_yml = generate_diff(file_path1_json, file_path2_yml)
+        assert res_json_yml == expected
