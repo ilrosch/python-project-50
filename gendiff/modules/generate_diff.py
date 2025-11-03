0da914e73aa5b9e979f915d294b9188853bbe46e
ilya roschikhmarov <ilrosch@mail.ru>
2025-11-03 13:35:59 +0300

    fix tests

--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -10,7 +10,9 @@ def generate_diff(file_path1: str, file_path2: str, format: str = "stylish"):
         data_a = parse_file(file_path1)
         data_b = parse_file(file_path2)
         diff_tree = build_diff_tree(data_a, data_b)
-        return formatter(format, diff_tree)
+        result = formatter(format, diff_tree)
+        print(result)
+        sys.exit(0)
     except Exception as err:
         print(err)
         sys.exit(1)
